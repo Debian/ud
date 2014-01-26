db79026eac497493f12d1a90e088921c335f5b38
Luca Filipozzi <lfilipoz@emyr.net>
2014-01-26 21:12:31 +0000

    enable inet mode and don't require a config file Signed-off-by: Luca Filipozzi <lfilipoz@emyr.net>

--- src/common/management/commands/fingerd.py
+++ src/common/management/commands/fingerd.py
@@ -26,98 +26,101 @@ from django.utils.translation import ugettext as _
 from common.models import DebianUser
 
 import daemon
+import grp
 import io
 import optparse
-import yaml
 import pwd
-import grp
+import sys
+import yaml
 
 import SocketServer
 
 from _utilities import load_configuration_file
 
-class FingerServer(SocketServer.TCPServer):
-    allow_reuse_address = True
+class GenericHandler(object):
+    def handle(self, uid):
+        fd = io.StringIO()
+        if uid.endswith('/key'):
+            uid = uid[:-4]
+            user = DebianUser.objects.get(uid=uid)
+            if user:
+                asc = user.key
+                if asc:
+                    fd.write(user.dn + '\n')
+                    fd.write(asc)
+                else:
+                    raise Exception('public key for "%s" not found at db.debian.org\n' % (uid))
+            else:
+                raise Exception('ldap entry for "%s" not found at db.debian.org\n' % (uid))
+        else:
+            user = DebianUser.objects.get(uid=uid)
+            if user:
+                fd.write(u'%s\n' % (user.dn))
+                fd.write(u'First name: %s\n' % (user.cn))
+                if user.mn:
+                    fd.write(u'Middle name: %s\n' % (user.mn))
+                fd.write(u'Last name: %s\n' % (user.sn))
+                fd.write(u'Email: %s\n' % (user.emailAddress))
+                if user.labeledURI:
+                    fd.write(u'URL: %s\n' % (user.labeledURI))
+                if user.ircNick:
+                    fd.write(u'IRC nickname: %s\n' % (user.ircNick))
+                if user.icqUin:
+                    fd.write(u'ICQ UIN: %s\n' % (user.icqUin))
+                if user.jabberJID:
+                    fd.write(u'Jabber ID: %s\n' % (user.jabberJID))
+                if user.keyFingerPrint:
+                    fd.write(u'Fingerprint: %s\n' % (user.keyFingerPrint))
+                    fd.write(u'Key block: finger %s/key@db.debian.org\n' % (user.uid))
+            else:
+                raise Exception('ldap entry for "%s" not found at db.debian.org\n' % (uid))
+        return fd.getvalue()
 
-class FingerHandler(SocketServer.StreamRequestHandler):
+class FingerHandler(SocketServer.StreamRequestHandler, GenericHandler):
     def handle(self):
         try:
-            fd = io.StringIO(encoding='utf-8')
             uid = self.rfile.readline(512).strip()
-            if uid.endswith('/key'):
-                uid = uid[:-4]
-                user = DebianUser.objects.get(uid=uid)
-                if user:
-                    asc = user.key
-                    if asc:
-                        fd.write(user.dn + '\n')
-                        fd.write(asc)
-                    else:
-                        raise Exception('public key for "%s" not found at db.debian.org\n' % (uid))
-                else:
-                    raise Exception('ldap entry for "%s" not found at db.debian.org\n' % (uid))
-            else:
-                user = DebianUser.objects.get(uid=uid)
-                if user:
-                    fd.write(u'%s\n' % (user.dn))
-                    fd.write(u'First name: %s\n' % (user.cn))
-                    if user.mn:
-                        fd.write(u'Middle name: %s\n' % (user.mn))
-                    fd.write(u'Last name: %s\n' % (user.sn))
-                    fd.write(u'Email: %s\n' % (user.emailAddress))
-                    if user.labeledURI:
-                        fd.write(u'URL: %s\n' % (user.labeledURI))
-                    if user.ircNick:
-                        fd.write(u'IRC nickname: %s\n' % (user.ircNick))
-                    if user.icqUin:
-                        fd.write(u'ICQ UIN: %s\n' % (user.icqUin))
-                    if user.jabberJID:
-                        fd.write(u'Jabber ID: %s\n' % (user.jabberJID))
-                    if user.keyFingerPrint:
-                        fd.write(u'Fingerprint: %s\n' % (user.keyFingerPrint))
-                        fd.write(u'Key block: finger %s/key@db.debian.org\n' % (user.uid))
-                else:
-                    raise Exception('ldap entry for "%s" not found at db.debian.org\n' % (uid))
-            self.wfile.write(fd.getvalue().encode('utf-8'))
+            self.wfile.write(GenericHandler.handle(self, uid).encode('utf-8'))
         except Exception as err:
             self.wfile.write(err)
+
+class FingerServer(SocketServer.TCPServer):
+    allow_reuse_address = True
 
 class Command(BaseCommand):
     help = _('Provides a finger daemon.')
     option_list = BaseCommand.option_list + (
+        optparse.make_option('--inetd',
+            action='store_true',
+            default=False,
+            help=_('run from inetd')
+        ),  
         optparse.make_option('--foreground',
             action='store_true',
             default=False,
             help=_('run in the foreground')
         ),
-        optparse.make_option('--config',
-            action='store',
-            default='/etc/ud/fingerd.yaml',
-            help=_('specify configuration file')
-        ),  
     )
 
     def handle(self, *args, **options):
         self.options = options
         try:
-            load_configuration_file(self.options['config'])
-            if settings.config.has_key('username'):
-                if settings.config['username'].endswith(DebianUser.base_dn):
-                    settings.DATABASES['ldap']['USER'] = settings.config['username']
-                else:
-                    settings.DATABASES['ldap']['USER'] = 'uid=%s,%s' % (settings.config['username'], DebianUser.base_dn)
-            else:
-                raise CommandError(_('configuration file must specify username parameter'))
-            if settings.config.has_key('password'):
-                settings.DATABASES['ldap']['PASSWORD'] = settings.config['password']
-            else:
-                raise CommandError(_('configuration file must specify password parameter'))
-            server = FingerServer(('', 79), FingerHandler)
-            if self.options['foreground']:
-                server.serve_forever()
-            else:
-                with daemon.DaemonContext(uid=pwd.getpwnam('nobody').pw_uid, gid=grp.getgrnam('nogroup').gr_gid):
+            if self.options['inetd']:
+                try:
+                    handler = GenericHandler()
+                    uid = sys.stdin.readline(512).strip()
+                    sys.stdout.write(handler.handle(uid).encode('utf-8'))
+                except Exception as err:
+                    sys.stdout.write('an error has occurred')
+                finally:
+                    sys.stdout.flush()
+            else: # run as daemon
+                server = FingerServer(('', 79), FingerHandler)
+                if self.options['foreground']:
                     server.serve_forever()
+                else:
+                    with daemon.DaemonContext(uid=pwd.getpwnam('nobody').pw_uid, gid=grp.getgrnam('nogroup').gr_gid):
+                        server.serve_forever()
         except Exception as err:
             raise CommandError(err)
 
